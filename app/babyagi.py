f01e69fe67b6b2e9c239412062b5f24e3d3ae4ea
Derek David <derekmiracledavid@hotmail.com>
2023-04-15 18:03:44 +0100

    task 1

---
+++ app/babyagi.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python3
+import os
+import openai
+import pinecone
+import time
+import sys
+from collections import deque
+from typing import Dict, List
+from dotenv import load_dotenv
+import os
+from twilio.twiml.messaging_response import MessagingResponse
+
+# Set Variables
+load_dotenv()
+
+resp = MessagingResponse()
+# Set API Keys
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+
+# Use GPT-3 model
+USE_GPT4 = False
+if USE_GPT4:
+    print("\033[91m\033[1m" +
+          "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"+"\033[0m\033[0m")
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
+assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
+
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "us-east1-gcp")
+assert PINECONE_ENVIRONMENT, "PINECONE_ENVIRONMENT environment variable is missing from .env"
+
+# Table config
+YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
+assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
+
+# Project config
+# OBJECTIVE = sys.argv[1] if len(sys.argv) > 1 else os.getenv("OBJECTIVE", "")
+# assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
+
+YOUR_FIRST_TASK = os.getenv("FIRST_TASK", "")
+assert YOUR_FIRST_TASK, "FIRST_TASK environment variable is missing from .env"
+
+# Print OBJECTIVE
+# print("\033[96m\033[1m"+"\n*****OBJECTIVE*****\n"+"\033[0m\033[0m")
+# print(OBJECTIVE)
+
+# Configure OpenAI and Pinecone
+openai.api_key = OPENAI_API_KEY
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+
+# Create Pinecone index
+table_name = YOUR_TABLE_NAME
+dimension = 1536
+metric = "cosine"
+pod_type = "p1"
+if table_name not in pinecone.list_indexes():
+    pinecone.create_index(table_name, dimension=dimension,
+                          metric=metric, pod_type=pod_type)
+
+# Connect to the index
+index = pinecone.Index(table_name)
+
+# Task list
+task_list = deque([])
+
+
+def add_task(task: Dict):
+    task_list.append(task)
+
+
+def get_ada_embedding(text):
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+
+
+def openai_call(prompt: str, use_gpt4: bool = False, temperature: float = 0.5, max_tokens: int = 100):
+    if not use_gpt4:
+        # Call GPT-3 DaVinci model
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        return response.choices[0].text.strip()
+    else:
+        # Call GPT-4 chat model
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+        )
+        return response.choices[0].message.content.strip()
+
+
+def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str], gpt_version: str = 'gpt-3'):
+    prompt = f"You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective}, The last completed task has the result: {result}. This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array."
+    response = openai_call(prompt, USE_GPT4)
+    new_tasks = response.split('\n')
+    return [{"task_name": task_name} for task_name in new_tasks]
+
+
+def prioritization_agent(this_task_id: int, objective, gpt_version: str = 'gpt-3'):
+    global task_list
+    task_names = [t["task_name"] for t in task_list]
+    next_task_id = int(this_task_id)+1
+    prompt = f"""You are an task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}. Consider the ultimate objective of your team:{objective}. Do not remove any tasks. Return the result as a numbered list, like:
+    #. First task
+    #. Second task
+    Start the task list with number {next_task_id}."""
+    response = openai_call(prompt, USE_GPT4)
+    new_tasks = response.split('\n')
+    task_list = deque()
+    for task_string in new_tasks:
+        task_parts = task_string.strip().split(".", 1)
+        if len(task_parts) == 2:
+            task_id = task_parts[0].strip()
+            task_name = task_parts[1].strip()
+            task_list.append({"task_id": task_id, "task_name": task_name})
+
+
+def execution_agent(objective: str, task: str, gpt_version: str = 'gpt-3') -> str:
+    #context = context_agent(index="quickstart", query="my_search_query", n=5)
+    context = context_agent(index=YOUR_TABLE_NAME, query=objective, n=5)
+    #print("\n*******RELEVANT CONTEXT******\n")
+    # print(context)
+    prompt = f"You are an AI who performs one task based on the following objective: {objective}.\nTake into account these previously completed tasks: {context}\nYour task: {task}\nResponse:"
+    return openai_call(prompt, USE_GPT4, 0.7, 2000)
+
+
+def context_agent(query: str, index: str, n: int):
+    query_embedding = get_ada_embedding(query)
+    index = pinecone.Index(index_name=index)
+    results = index.query(query_embedding, top_k=n,
+                          include_metadata=True)
+    #print("***** RESULTS *****")
+    # print(results)
+    sorted_results = sorted(
+        results.matches, key=lambda x: x.score, reverse=True)
+    return [(str(item.metadata['task'])) for item in sorted_results]
+
+
+async def process_task_telegram(objective: str, base_url: str, chat_id: str, client=None):
+    first_task = {
+        "task_id": 1,
+        "task_name": YOUR_FIRST_TASK
+    }
+
+    add_task(first_task)
+    # Main loop
+    task_id_counter = 1
+    if client is not None:
+        while True:
+            if task_list:
+                # Print the task list
+                print("\033[95m\033[1m" +
+                      "\n*****TASK LIST*****\n"+"\033[0m\033[0m")
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text=*****TASK LIST*****\n")
+                temp = ""
+                for t in task_list:
+                    tsk = str(t['task_id'])+": "+t['task_name']
+                    print(tsk)
+                    temp = temp + tsk + "\n"
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text={temp}")
+
+                # Step 1: Pull the first task
+                task = task_list.popleft()
+                print("\033[92m\033[1m" +
+                      "\n*****NEXT TASK*****\n"+"\033[0m\033[0m")
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text=\n*****NEXT TASK*****\n")
+                next_tsk = str(task['task_id'])+": "+task['task_name']
+                print(next_tsk)
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text={next_tsk}")
+
+                # Send to execution function to complete the task based on the context
+                result = execution_agent(objective, task["task_name"])
+                this_task_id = int(task["task_id"])
+                print("\033[93m\033[1m" +
+                      "\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text=\n*****TASK RESULT*****\n")
+                print(result)
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text={result}")
+
+                # Step 2: Enrich result and store in Pinecone
+                # This is where you should enrich the result if needed
+                enriched_result = {'data': result}
+                result_id = f"result_{task['task_id']}"
+                # extract the actual result from the dictionary
+                vector = enriched_result['data']
+                index.upsert([(result_id, get_ada_embedding(vector), {
+                    "task": task['task_name'], "result":result})])
+
+            # Step 3: Create new tasks and reprioritize task list
+            if task_id_counter < 11:
+                print(f"tt: {task_id_counter}")
+                new_tasks = task_creation_agent(objective, enriched_result, task["task_name"], [
+                                                t["task_name"] for t in task_list])
+
+                for new_task in new_tasks:
+                    task_id_counter += 1
+                    new_task.update({"task_id": task_id_counter})
+                    add_task(new_task)
+                prioritization_agent(this_task_id, objective)
+            if len(task_list) < 1:
+                print("Tasks completed")
+                await client.get(f"{base_url}/sendMessage?chat_id={chat_id}&text=\n\nTask completed")
+                break
+
+
+async def process_task(objective: str):
+    # Add the first task
+    first_task = {
+        "task_id": 1,
+        "task_name": YOUR_FIRST_TASK
+    }
+
+    add_task(first_task)
+    # Main loop
+    task_id_counter = 1
+
+    temp_str = ""
+    while True:
+        if task_list:
+            # Print the task list
+            print("\033[95m\033[1m" +
+                  "\n*****TASK LIST*****\n"+"\033[0m\033[0m")
+            # resp.message(
+            #     "*****TASK LIST*****\n")
+            temp_str += "\n*****TASK LIST*****\n"
+            temp = ""
+            for t in task_list:
+                tsk = str(t['task_id'])+": "+t['task_name']
+                print(tsk)
+                temp = temp + tsk + "\n"
+            temp_str += temp
+            # resp.message(
+            #     f"{temp}")
+
+            # Step 1: Pull the first task
+            task = task_list.popleft()
+            print("\033[92m\033[1m" +
+                  "\n*****NEXT TASK*****\n"+"\033[0m\033[0m")
+            # resp.message(
+            #     f"*****NEXT TASK*****\n")
+            temp_str += "*****NEXT TASK*****\n"
+            next_tsk = str(task['task_id'])+": "+task['task_name'] + "\n"
+            print(next_tsk)
+            # resp.message(
+            #     f"{next_tsk}")
+            temp_str += next_tsk
+
+            # Send to execution function to complete the task based on the context
+            result = execution_agent(objective, task["task_name"])
+            this_task_id = int(task["task_id"])
+            print("\033[93m\033[1m" +
+                  "\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
+            # resp.message(
+            #     f"*****TASK RESULT*****\n")
+            temp_str += "*****TASK RESULT*****\n"
+            print(result)
+            # resp.message(
+            #     f"{result}")
+            temp_str += result + "\n"
+
+            # Step 2: Enrich result and store in Pinecone
+            # This is where you should enrich the result if needed
+            enriched_result = {'data': result}
+            result_id = f"result_{task['task_id']}"
+            # extract the actual result from the dictionary
+            vector = enriched_result['data']
+            index.upsert([(result_id, get_ada_embedding(vector), {
+                "task": task['task_name'], "result":result})])
+
+        # Step 3: Create new tasks and reprioritize task list
+        if task_id_counter < 11:
+            print(f"tt: {task_id_counter}")
+            new_tasks = task_creation_agent(objective, enriched_result, task["task_name"], [
+                                            t["task_name"] for t in task_list])
+
+            for new_task in new_tasks:
+                task_id_counter += 1
+                new_task.update({"task_id": task_id_counter})
+                add_task(new_task)
+            prioritization_agent(this_task_id, objective)
+        if len(task_list) < 1:
+            print("Tasks completed")
+            resp.message(
+                f"Task completed")
+            break
+    return temp_str
+
+    # time.sleep(1)  # Sleep before checking the task list again
--- app/twilio_handler.py
+++ app/twilio_handler.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Form, Response
 from twilio.twiml.messaging_response import MessagingResponse
 from chat_handler import process_chat_message
 from voice_handler import process_voice_message
+from babyagi import process_task
 
 twilio_api_reply = APIRouter()
 
@@ -23,17 +24,23 @@ async def handle_twilio_api_reply(Body: str = Form(""), MediaUrl0: str = Form(""
         output = await process_voice_message(MediaUrl0, 0)
     else:
         # Process text messages
+
+        resp = MessagingResponse()
+        text = Body.lower()
+        if text[0:5] == "/task":
+            task = text[5:]
+            responses = await process_task(task)
+            response_msg = resp.message(responses)
+            return Response(content=str(resp), media_type="application/xml")
+
         output = await process_chat_message(Body, 0)
 
-    print(output)
-    resp = MessagingResponse()
-    
-    # Send the output as a text message or a photo with a caption, depending on the type of output
-    if isinstance(output, tuple):
-        summary, image = output
-        response_msg = resp.message(summary)
-        response_msg.media(image)
-    else:
-        response_msg = resp.message(output)
+        # Send the output as a text message or a photo with a caption, depending on the type of output
+        if isinstance(output, tuple):
+            summary, image = output
+            response_msg = resp.message(summary)
+            response_msg.media(image)
+        else:
+            response_msg = resp.message(output)
 
-    return Response(content=str(resp), media_type="application/xml")
+        return Response(content=str(resp), media_type="application/xml")
